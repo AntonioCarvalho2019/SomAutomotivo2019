5f95fce153285ce25dfb5629a5eda969ff3acf2d
AntonioCarvalho2019 <antoniotonys2014@gmail.com>
2023-01-18 23:46:05 -0300

    ajuste

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const server = express()
-
-server.get('/', () =>{
-    console.log('Estamos no caminho certo !!')
-})
-
-server.listen(3333)
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const server = express();
-
-server.get('/', (req, res) =>{
-    return res.json({msg: "hello world"});
-});
