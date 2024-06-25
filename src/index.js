65f004b48cb0dbb6727414c15ce84539ba02160d
AntonioCarvalho2019 <antoniotonys2014@gmail.com>
2024-06-24 22:00:55 -0300

    ajuste novo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 
 server.use((req, res) => {
-  res.send('Estou criando uma nova :) 2024')
+  res.send('Retorno estudos nodejs 2024')
 })
 
 const PORT = process.env.PORT || 3000
